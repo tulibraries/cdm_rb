e2440a7768fd2c7354e35566d1f07baefcb2ef3d
Chad Nelson <chad.nelson@temple.edu>
2016-03-30 14:55:02 -0400

    Flesh out requesting info for single item

--- lib/cdm/api/item_info.rb
+++ lib/cdm/api/item_info.rb
@@ -2,6 +2,39 @@ module CDM
   module Api
     class ItemInfo < ApiClient
 
+      def local_init(args)
+        @collection = args[:collection]
+        @id         = args[:id]
+        @item       = get_item(args)
+      end
+
+      def get_item(args)
+        Nokogiri.XML request(args)
+      end
+
+      def request(args)
+        format = set_format args.fetch(:format, nil)
+        open("#{@base_url}dmGetItemInfo/#{@collection}/#{@id}/#{format}")
+      end
+
+      def raw
+        @item.to_xml
+      end
+
+      def parsed
+        @item
+      end
+
+      def to_h
+        item_hash = {}
+        @item.xpath('/xml').children.each do |child|
+          item_hash[child.name] = child.text
+        end
+        item_hash
+      end
+
+      #@TODO HANDLE UNKNOWN ITEMS GRACEFULLY
     end
   end
 end
+
--- spec/api/item_info_spec.rb
+++ spec/api/item_info_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.configure do |config|
   config.before(:each) do
-    stub_request(:get, /example.com/)
+    stub_request(:get, /dmwebservices\/index\.php\?q=dmGetItemInfo/)
         .to_return(
             status: 200,
             body: File.open(SPEC_ROOT + '/fixtures/item_info_response.xml').read ,
@@ -14,14 +14,35 @@ end
 describe 'ContentDM Item Info Query' do
   describe 'Returns representations of single item' do
     before do
-      @item_info = CDM::Api::ItemInfo.new :url => 'http://example.com', :collection => 'p16002coll9', :id => '1314'
+      @item = CDM::Api::ItemInfo.new :url => 'http://example.com', :collection => 'p16002coll9', :id => '1314'
     end
 
-    context 'list method' do
-      it 'returns a list of collections' do
-        expect(@collections.list).to be_an Array
+    context 'raw method' do
+      it 'returns the raw xml for a single item' do
+        expect(@item.raw).to be_a String
+      end
+
+      it 'returns the raw xml for a single item' do
+        expect(@item.raw).to include '<title>3 questions to employers</title>'
       end
     end
+
+    context 'parsed method' do
+      it 'returns a Nokogiri object' do
+        expect(@item.parsed).to be_a Nokogiri::XML::Document
+      end
+    end
+
+    context 'hash method' do
+      it 'returns a hash of the records fields' do
+        expect(@item.to_h).to be_a Hash
+      end
+
+      it 'has expected keys' do
+        expect(@item.to_h.keys).to include 'reposa', 'title'
+      end
+    end
+
   end
 end
 
