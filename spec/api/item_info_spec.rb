d86e8c727bff954f0392f2c478c6f18f305c557f
Chad Nelson <chad.nelson@temple.edu>
2016-03-30 16:34:57 -0400

    More tests for item info query.

--- spec/api/item_info_spec.rb
+++ spec/api/item_info_spec.rb
@@ -18,12 +18,16 @@ describe 'ContentDM Item Info Query' do
     end
 
     context 'raw method' do
+      before do
+        @raw = @item.raw
+
+      end
       it 'returns the raw xml for a single item' do
-        expect(@item.raw).to be_a String
+        expect(@raw).to be_a String
       end
 
       it 'returns the raw xml for a single item' do
-        expect(@item.raw).to include '<title>3 questions to employers</title>'
+        expect(@raw).to include '<title>3 questions to employers</title>'
       end
     end
 
@@ -34,12 +38,33 @@ describe 'ContentDM Item Info Query' do
     end
 
     context 'hash method' do
+      before do
+        @hash = @item.to_h
+      end
       it 'returns a hash of the records fields' do
-        expect(@item.to_h).to be_a Hash
+        expect(@hash).to be_a Hash
       end
 
       it 'has expected keys' do
-        expect(@item.to_h.keys).to include 'reposa', 'title'
+        expect(@hash.keys).to include 'reposa', 'title', 'ada', 'cdmisnewspaper'
+      end
+
+      context 'has expected values for fields' do
+        it 'has expected values for fields with data' do
+          expect(@hash['title']).to eql '3 questions to employers'
+        end
+
+        it 'has anb empty value for empty fields' do
+          expect(@hash['dmaccess']).to eql ''
+        end
+
+        it 'handles dates as strings' do
+          expect(@hash['dmmodified']).to eql '2012-09-11'
+        end
+
+        it 'handles integers as strings' do
+          expect(@hash['cdmisnewspaper']).to eql '0'
+        end
       end
     end
 
