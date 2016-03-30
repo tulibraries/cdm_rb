f6135271f41cf659e8a813d3a9eeeb16292d636b
Chad Nelson <chad.nelson@temple.edu>
2016-03-30 08:59:45 -0400

    Add simplecov and rubocop

--- cdm.gemspec
+++ cdm.gemspec
@@ -33,4 +33,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "webmock"
+  spec.add_development_dependency 'rubocop', '~> 0.39.0'
+  spec.add_development_dependency 'simplecov'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,6 +45,8 @@ RSpec.configure do |config|
   require 'webmock/rspec'
   WebMock.disable_net_connect!(allow_localhost: true)
 
+  require 'simplecov'
+  SimpleCov.start
 
   # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
