163d9048b6a75b20ff7dee6decfc6e7bac2154c9
Chad Nelson <chad.nelson@temple.edu>
2016-04-19 11:17:58 -0400

    Reorganize base api

--- lib/cdm.rb
+++ lib/cdm.rb
@@ -1,6 +1,6 @@
 require 'cdm/version'
 require 'cdm/api/urls'
-require 'cdm/api_client'
+require 'cdm/api/api_client'
 require 'cdm/api/collections'
 require 'cdm/api/item_info'
 require 'cdm/api/search_query'
